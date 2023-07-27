c2869de47e00af5c5500f73ccdeeef289f1ae135
Y56380X <y56380x@users.noreply.github.com>
2023-07-27 18:27:45 +0200

    add method extension class

---
+++ src/LinqApplyIf/ApplyIfExtensions.cs
@@ -0,0 +1,7 @@
+namespace LinqApplyIf
+{
+    /// <summary>
+    /// Providing extenstion methods to apply transformations conditionally on enumerations.
+    /// </summary>
+    public static class ApplyIfExtensions { }
+}
